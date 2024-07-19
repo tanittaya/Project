df59755f3bb4003bdd7dbda9c5b590b54d4beb0f
tanittaya <152164438+tanittaya@users.noreply.github.com>
2024-07-19 13:19:20 +0700

    FN

--- vannarong/vannarong/js/scridt.js
+++ vannarong/vannarong/js/scridt.js
@@ -9,4 +9,10 @@ $(function() {
             text.addClass("hidden");
         }
     });
+<<<<<<< HEAD
+  });
+  $(window).on('load',function(){
+    //$('#bg').addClass('loaded');
+=======
+>>>>>>> 77856d6dc2d9980bf6fb4e69e2b9f2e3ef9f8807
   });
